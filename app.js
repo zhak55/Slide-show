68ae383e7095e80c0bff6be0cb851505a4a66c99
Roman Zhak <zhak55@ya.ru>
2015-01-14 17:06:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app     = express();
 
 app.use(express.static(__dirname + '/'));
-
+// just for test ajax
 app.get('/', function(req, res){
   res.send('/index.html');
 });
